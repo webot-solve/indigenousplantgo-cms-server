98d4e66e224329a595146ae634bc7f85ff26654c
Wayne Lee <waynel1996weiyang@gmail.com>
2021-04-27 23:31:31 -0700

    Revision fix

--- mongoDatabase.js
+++ mongoDatabase.js
@@ -576,7 +576,7 @@ module.exports = async function() {
     }
 
     const result = await revisions.insertOne({
-      user_id: ObjectID(user_id),
+      user: ObjectID(user_id),
       date: Date.now()
     })
     return result
